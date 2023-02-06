a67ab9d21865a771126fcb773387ba198e532594
porsolic <porsolic@github>
2023-02-06 19:27:09 +0100

    Fix build on FreeBSD

--- tools/blisp/src/util.h
+++ tools/blisp/src/util.h
@@ -4,9 +4,7 @@
 
 #include <stdint.h>
 
-#ifdef __linux__
-#include <unistd.h>
-#elif defined(_MSC_VER)
+#if defined(_MSC_VER)
 #include <BaseTsd.h>
 typedef SSIZE_T ssize_t;
 #include <windows.h>
@@ -15,6 +13,8 @@ typedef SSIZE_T ssize_t;
 #include <sys/syslimits.h>
 #include <assert.h>
 #include <sys/types.h>
+#else
+#include <unistd.h>
 #endif
 
 ssize_t util_get_binary_folder(char* buffer, uint32_t buffer_size);
